d391f2f4fa679fb843e058b34a15440cc9f53d61
Egor Glebov <beast.loby@gmail.com>
2017-05-14 12:05:52 +0300

    add(theme) checkout

--- src/js/templates/checkout.js
+++ src/js/templates/checkout.js
@@ -1,121 +1,108 @@
 //hide not needed
-//$("#registered_client").detach();
-//$("#contacts").detach();
-//$(".checkout__fieldset h3").detach();
-//$(".checkout__fieldset h4").detach();
-//$(".checkout__fieldset .small").detach();
-//$(".checkout__fieldset .warning").detach();
-  //temporary
+$("#registered_client").detach();
+$("#contacts").detach();
+$(".checkout__fieldset h3").detach();
+$(".checkout__fieldset h4").detach();
+$(".checkout__fieldset .small").detach();
+$(".checkout__fieldset .warning").detach();
+$(".delivery_variants .co-input-wrapper label:last-child").detach();
+//$(".payment_variants .co-input-wrapper label:last-child").detach();
+//  temporary
 //  $(".delivery_variants").detach();
 //  $(".payment_variants").detach();
 //  $(".checkout__fieldset .error").detach();
 
-//add classes
-//$(function(){
-//  $("#order_form input").each(
-//    function(){
-//      $(this).addClass("form__input")
-//    }
-//  )
-//});
-//$(".checkout__fieldset")
-//  .html("<div class='test'></div>");
+//error
+$(".checkout__fieldset--generate .error")
+  .addClass("error-message")
+;
 
-//$("#regular_client .field.fc")
-//  .prependTo("<div class='test'></div>")
-//  .prependTo("#order_form");
+$(".checkout__fieldset--generate .error br:first-of-type")
+  .detach()
+;
 
-//$("#regular_client .field.fc")
-//  .insertBefore($("#regular_client"));
+$(".checkout__fieldset--generate .error-message a")
+  .addClass("link")
+  .css({
+    "text-align": "center"
+  })
+;
 
-//$(".checkout__fieldset--generate")
-//  .html("<div class='form__row'></div>")
+//form
+$(".checkout__fieldset--generate label")
+  .unwrap()
+  .addClass("form__label")
+;
 
-//$("#regular__client label")
-//  .appendTo("<div class='form__item'></div>")
-//  .appendTo("#regular__client")
-//;
-//$("<div class='test'></div>")
-//  .replaceAll("#regular_client .field.fc");
+$(".checkout__fieldset--generate input")
+  .unwrap()
+  .addClass("form__input")
+;
 
+$(".checkout__fieldset--generate .field.fc")
+  .addClass("form__item")
+  .removeClass("field fc")
+  .unwrap()
+;
 
-//$("#regular_client label")
-//  .addClass("form__label")
-//  .prependTo("#order_form")
-//  .wrap("<div class='form__item  form__item--1-of-2'></div>")
-//;
-//
-//$("#regular_client input")
-//  .addClass("form__input")
-//  .appendTo("#order_form .form__item")
-//;
+$(".checkout__fieldset--generate textarea")
+  .addClass("form__input  form__input--textarea")
+  .unwrap()
+  .css("width", "100%")
+;
 
-//$("#order_form")
-//  .prepend("<div class='form__row'><div class='form__item form__item-1-of-2'></div></div>")
-//;
-//
-//$("#regular_client label, #regular_client input")
-//  .prependTo(".form__item")
-//;
+$(".checkout__fieldset--generate .field-label")
+  .css({
+    "width": "100%",
+    "margin-bottom": "8px"
+  })
+;
 
+$(".checkout__fieldset--generate select")
+  .addClass("form__input")
+  .unwrap()
+;
 
+$(".checkout__fieldset--generate input[type='submit']")
+  .removeClass("button big form__input")
+  .addClass("form__submit")
+  .css("margin", "30px 0 0 0")
+;
 
-//$(".checkout__fieldset--generate label")
-//  .unwrap()
-//  .addClass("form__label")
-//;
-//
-//$(".checkout__fieldset--generate input")
-//  .unwrap()
-//  .addClass("form__input")
-//;
-//
-//$(".checkout__fieldset--generate .field.fc")
-//  .addClass("form__item")
-//  .removeClass("field fc")
-//  .unwrap()
-//;
-//
-//$(".checkout__fieldset--generate textarea")
-//  .addClass("form__input  form__input--textarea")
-//  .unwrap()
-//  .css("width", "100%")
-//;
-//
-//$(".checkout__fieldset--generate .field-label")
-//  .css({
-//    "width": "100%",
-//    "margin-bottom": "8px"
-//  })
-//;
-//
-//$(".checkout__fieldset--generate select")
-//  .addClass("form__input")
-//  .unwrap()
-//;
-//
-//$(".checkout__fieldset--generate input[type='submit']")
-//  .removeClass("button big form__input")
-//  .addClass("form__submit")
-//  .css("margin", "30px 0 0 0")
-//;
-//
-//$("label[for='client_name'], label[for='client_surname'], label[for='client_phone'], label[for='shipping_address_country'], label[for='shipping_address_city'], label[for='shipping_address_zip'], label[for='shipping_address_adress']")
-//  .addClass("form__label--require")
-//;
+$("label[for='client_name'], label[for='client_surname'], label[for='client_phone'], label[for='shipping_address_country'], label[for='shipping_address_city'], label[for='shipping_address_zip'], label[for='shipping_address_adress']")
+  .addClass("form__label--require")
+;
 
+//delivery
 
+$(".delivery_variants br:nth-of-type(2n)")
+  .detach()
+;
 
+$(".delivery_variants label")
+  .addClass("form__label--delivery")
+;
 
-//$(".set-block .form__item:nth-of-type(-n+4)")
-//  .addClass("form__item--1-of-2")
-//;
+$(".delivery_variants p")
+  .addClass("checkout__delivery-descr")
+;
 
-//$(".set-block .form__item:nth-of-type(2n+1)")
-//  .appendTo("<div class='form__row'></div>")
-//  .wrap("<div class='form__row'></div>")
-//;
+//payment
 
-//$(".set-block .form__row + .form__item")
-//  .appendTo(".form__row")
-//;
+$(".payment_variants br:nth-of-type(2n)")
+  .detach()
+;
+
+$(".payment_variants label")
+  .addClass("form__label--delivery")
+;
+
+$(".payment_variants p")
+  .detach()
+;
+
+//delivery and payment
+
+$(".variants .not_available label")
+  .addClass("form__label--disabled")
+;
